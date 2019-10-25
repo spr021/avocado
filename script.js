d1ab3390d8ad0bdf40fa03cfc6591d70dc3dc4ea
spr021 <saber.pourrahimi.1999@gmail.com>
2019-10-26 02:25:22 +0330

    add profile view

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+function profileView(){
+    var user = document.getElementById('user-info-border')
+    console.log(user.classList.height)
+    document.getElementById("chating").style.display = 'none'
+    document.getElementById('user-info-border').className = 'user-info-border-flex'
+}
+function closeProfile(){
+        document.getElementById('user-info-border').className = 'user-info-border-none'
+        document.getElementById("chating").style.display = ''
+}
+function send(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault()
+        document.getElementById("send-pm").click()
+       }
+  }
 
 function chat() { 
     var massage = document.getElementById("my-talk")
     massage.value.trim()
-    console.log(massage)
         if (massage.value == "") {
         return false;
         }
@@ -10,8 +25,8 @@ function chat() {
             var chatBox = document.createElement('div')
             chatBox.className  = 'msg-me'
             chatBox.innerHTML = '<div class="talk-img"><img src="img/avatar/boy.png" /></div><div class="talk-msg">' + massage.value + '</div>'
-            document.getElementById("chat-text").appendChild(chatBox)
+            document.getElementById("chating").appendChild(chatBox)
             massage.value = ""
-            document.getElementById("chat-text").scrollBy(0, 1000)
+            document.getElementById("chating").scrollBy(0, 1000)
         }
-}
+    }
